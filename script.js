2dd6905e4ced06b30e9e6199233d645d7176028e
Sebastian-Torres-Matrix <52598263+Sebastian-Torres-Matrix@users.noreply.github.com>
2021-02-28 01:17:35 +0100

    updated bug fix

--- script.js
+++ script.js
@@ -21,20 +21,18 @@ function showMovies(movies) {
     main.innerHTML = ''
 
     movies.forEach((movie) => {
-        const { title, poster_path, vote_average,
-            overview } = movie
+        const { title, poster_path, vote_average, overview } = movie
 
-        const movieEl = document.createElement
-            ('div')
+        const movieEl = document.createElement('div')
         movieEl.classList.add('movie')
 
         movieEl.innerHTML = `
-        <img src="${IMG_PATH + poster_path}" alt=${title}" />
-        <div class="movie-info">
+            <img src="${IMG_PATH + poster_path}" alt="${title}">
+            <div class="movie-info">
           <h3>${title}</h3>
           <span class="${getClassByRate(vote_average)}">${vote_average}</span>
-        </div>
-        <div class="overview">
+            </div>
+            <div class="overview">
           <h3>Overview</h3>
           ${overview}
         </div>
